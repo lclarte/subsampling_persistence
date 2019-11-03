d9abe48dfb53c594db185197b9804c2352486713
lucas <clarte.lucas@gmail.com>
2019-11-03 22:19:33 +0100

    added sampling functions

---
+++ sampling.py
@@ -0,0 +1,31 @@
+# sampling.py 
+
+import numpy as np
+
+def uniform_density(n):
+	return np.array([1./n]*n)
+
+
+def get_samples(X, mu, n):
+	"""
+	Prend en argument un ensemble X, une distribution et renvoie un nombre n de samples
+	"""
+	indexes = list(range(len(X)))
+	sampling = np.random.choice(indexes, p=mu, size=n, replace=False)
+	return X[sampling]
+
+
+def get_samples_from_density(X, mu, n):
+	"""
+	Prend en argument X, une fonction de densite mu et n samples
+	"""
+	probas = np.array([ mu(x) for x in X ])
+	if np.amax(probas) == 0:
+		return None 
+	probas /= np.linalg.norm(probas, ord=1)
+	return get_samples(X, probas, n)
+
+N = 10
+X = np.array([-1.0, 0.0, 0.5, 1.5])
+density = lambda x : float(0.0 <= x < 1)
+print(get_samples_from_density(X, density, 10))
