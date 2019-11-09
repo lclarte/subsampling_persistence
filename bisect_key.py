444858f0fb5cfbb5ce4c87cb5101eb5217ea54b8
ttk <clarte.lucas@gmail.com>
2019-11-09 15:59:21 +0100

    added bisect_left to bisect_key.py

--- bisect_key.py
+++ bisect_key.py
@@ -21,3 +21,23 @@ def insort_left(a, x, lo=0, hi=None, key= lambda x : x):
         else: hi = mid
     a.insert(lo, x)
     return lo
+
+def bisect_left(a, x, lo=0, hi=None, key= lambda x : x):
+    """Return the index where to insert item x in list a, assuming a is sorted.
+    The return value i is such that all e in a[:i] have e < x, and all e in
+    a[i:] have e >= x.  So if x already appears in the list, a.insert(x) will
+    insert just before the leftmost x already there.
+    Optional args lo (default 0) and hi (default len(a)) bound the
+    slice of a to be searched.
+    """
+
+    if lo < 0:
+        raise ValueError('lo must be non-negative')
+    if hi is None:
+        hi = len(a)
+    while lo < hi:
+        mid = (lo+hi)//2
+        # Use __lt__ to match the logic in list.sort() and in heapq
+        if key(a[mid]) < key(x): lo = mid+1
+        else: hi = mid
+    return lo
